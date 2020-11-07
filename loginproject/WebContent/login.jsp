e264541765e54b1eea484b9875f493df50e68ecc
vaishnavi586 <machavolu1996@gmail.com>
2020-11-07 15:37:46 +0530

    displayed login application using servlet

---
+++ loginproject/WebContent/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC ">
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+    <form action= "Login">
+    enter username : <input type="text" name="uname"><br>
+    enter password : <input type="password" name="pass"><br>
+    <input type="submit" value="login">
+    </form>
+</body>
+</html>
---
+++ loginproject/src/com/Login/Login.java
@@ -0,0 +1,32 @@
+package com.Login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		if(uname.equals("mvs")&& pass.equals("demo"))
+		{
+			HttpSession session = request.getSession();
+			 session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+			
+		}
+		else
+		{
+			
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+}	
